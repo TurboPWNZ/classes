0f476bffd26639bb9dabd4f303a005701019f9a9
Sergey Kaspruk <s.kaspryk@emict.com>
2015-07-15 14:08:19 +0300

    Класс работы с БД

---
+++ src/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'db' => array(
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => 'root',
+        'database' => 'db_name',
+        'charset' => 'utf8',
+    ),
+);
